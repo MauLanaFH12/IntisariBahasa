4473f9c8888d998fa7564c6117763d1e237d001f
MaulanaFH <maulanafadia118@gmail.com>
2017-04-08 13:16:20 +0700

    Commit Project

---
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl42022242835/myapplication/AntonimFragment.java
@@ -0,0 +1,25 @@
+package id.sch.smktelkom_mlg.xirpl42022242835.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by MaulanaFH on 4/8/2017.
+ */
+
+public class AntonimFragment extends Fragment {
+
+    public AntonimFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/xirpl42022242835/myapplication/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl42022242835/myapplication/MainActivity.java
@@ -1,17 +1,18 @@
 package id.sch.smktelkom_mlg.xirpl42022242835.myapplication;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.NavigationView;
+        import android.support.design.widget.Snackbar;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.view.GravityCompat;
+        import android.support.v4.widget.DrawerLayout;
+        import android.support.v7.app.ActionBarDrawerToggle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,14 +24,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -78,21 +71,47 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+
+        Fragment fragment = null;
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_antonim) {
+            fragment = new AntonimFragment();
+            setTitle("Antonim");
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.kataawal) {
+            fragment = new KAFragment();
+            setTitle("Kata Awal");
 
-        } else if (id == R.id.nav_slideshow) {
+        }else if (id == R.id.katadasar) {
+            fragment = new KDFragment();
+            setTitle("Kata Dasar");
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.majas) {
+            fragment = new MajasFragment();
+            setTitle("Majas");
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.peribahasa) {
+            fragment = new PeribahasaFragment();
+            setTitle("Peribahasa");
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.sinonim) {
+            fragment = new SinonimFragment();
+            setTitle("Sinonim");
 
+        } else if (id == R.id.search) {
+            fragment = new SearchFragment();
+            setTitle("Search");
+
+        }else if (id == R.id.aboutus){
+            fragment = new AboutUs();
+            setTitle("About US");
         }
+
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container,fragment).commitNow();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
