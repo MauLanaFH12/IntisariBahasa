e577de931937e14be5d904e085cc509acf09b5ea
Zulfikri Rosyid <zulf354@gmail.com>
2017-04-09 20:53:35 +0700

    Searchbox Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/xirpl42022242835/myapplication/SearchFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl42022242835/myapplication/SearchFragment.java
@@ -1,10 +1,23 @@
 package id.sch.smktelkom_mlg.xirpl42022242835.myapplication;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Izza on 08/04/2017.
  */
 
 public class SearchFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.searchable, container, false);
+        return rootView;
+    }
+
+
 }
--- app/src/main/java/id/sch/smktelkom_mlg/xirpl42022242835/myapplication/SinonimFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl42022242835/myapplication/SinonimFragment.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import id.sch.smktelkom_mlg.xirpl42022242835.myapplication.model.Sinonim;
-
 /**
  * Created by MaulanaFH on 4/8/2017.
  */
@@ -32,8 +30,8 @@ public class SinonimFragment extends Fragment {
         return rootView;
     }
     public void Sinonim() {
+
         Intent intent = new Intent(getActivity(), SinonimActivity.class);
         startActivity(intent);
-
     }
 }
