3faff70feb2fc04debd729de0e8affc4b221b31b
Angga Ramadhan <ramadhan.angga06@gmail.com>
2017-04-24 18:25:36 +0700

    isi kd

---
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl42022242835/myapplication/model/KataDasar.java
@@ -0,0 +1,20 @@
+package id.sch.smktelkom_mlg.xirpl42022242835.myapplication.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Angga on 4/24/2017.
+ */
+
+public class KataDasar implements Serializable {
+
+    public String judul;
+
+
+    public KataDasar(String judul) {
+        this.judul = judul;
+
+    }
+
+
+}
